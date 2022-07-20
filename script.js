f5af603b525f640ed3b2d4e68470333630cb7ca0
Sunil Kumar <sunilk10099@gmail.com>
2022-07-20 17:53:48 +0530

    add basic content

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const gameBoard = (() => {
+    const board = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]];
+    return {board};
+})();
+
+const displayController = (() => {
+
+})();
+
+const players = () => {
+
+};
