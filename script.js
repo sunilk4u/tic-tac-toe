c1d127af4ad688a046804cad21f2ee910183598e
Sunil Kumar <sunilk10099@gmail.com>
2022-07-22 22:45:02 +0530

    Fix bug event listener not removing

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ const players = (selectedPlayer, selectedSymbol, turn) => {
 };
 
 const gameBoard = (() => {
-  const board = [
-    ["X", "X", "X"],
-    ["X", "X", "X"],
-    ["X", "X", "X"],
+  let board = ["N", "N", "N", "N", "N", "N", "N", "N", "N"];
+  const winBoard = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   let player1, player2, otherSymbol;
 
@@ -32,16 +38,7 @@ const gameBoard = (() => {
   function startGame() {
     document.getElementById("res-display").innerHTML =
       "Lets's Begin! Your move first.";
-    const tileElements = Array.from(document.getElementsByClassName("tile"));
-    tileElements.forEach((tile) => {
-      tile.addEventListener(
-        "click",
-        (eventFunc = () => {
-          nextMove(tileElements.indexOf(tile) + 1);
-        }),
-        { once: true }
-      );
-    });
+    addListen();
     document.querySelector(".choose-player").disabled = true;
     document.querySelector(".choose-player").style["background-color"] =
       "#afafaf";
@@ -52,29 +49,88 @@ const gameBoard = (() => {
       //AI work here
     } else {
       if (player1.isTurn === true) {
+        board[index - 1] = player1.symbol;
         displayController.displayNextMove(index, player1);
-        displayController.changeTileColor(index, "#7cff7c")
+        displayController.changeTileColor(index, "#7cff7c");
         document.getElementById("res-display").innerHTML = "Player 2's Move";
         player1.isTurn = !player1.isTurn;
         player2.isTurn = !player2.isTurn;
-      }
-      else {
+      } else {
+        board[index - 1] = player2.symbol;
         displayController.displayNextMove(index, player2);
-        displayController.changeTileColor(index, "#ff7474")
+        displayController.changeTileColor(index, "#ff7474");
         document.getElementById("res-display").innerHTML = "Player 1's Move";
         player1.isTurn = !player1.isTurn;
         player2.isTurn = !player2.isTurn;
       }
     }
+    checkWin();
+  }
+
+  function checkWin() {
+    let X_Match = false,
+      O_Match = false;
+    winBoard.every((match) => {
+      if (
+        board[match[0]] === "X" &&
+        board[match[1]] === "X" &&
+        board[match[2]] === "X"
+      ) {
+        X_Match = true;
+        return false;
+      }
+      if (
+        board[match[0]] === "O" &&
+        board[match[1]] === "O" &&
+        board[match[2]] === "O"
+      ) {
+        O_Match = true;
+        return false;
+      }
+      return true;
+    });
+
+    if (X_Match === true) {
+      document.getElementById("res-display").innerHTML =
+        "Player 1 won the match";
+      removeListen();
+    }
+    if (O_Match === true) {
+      document.getElementById("res-display").innerHTML =
+        "Player 2 won the match";
+      removeListen();
+    }
+  }
+
+  const eventFunc = function () {
+    const divNum = this.getAttribute("data-attribute");
+    nextMove(divNum);
+  };
+
+  function addListen() {
+    const tileElements = Array.from(document.getElementsByClassName("tile"));
+    tileElements.forEach((tile) => {
+      tile.addEventListener("click", eventFunc, { once: true });
+    });
+  }
+
+  function removeListen() {
+    const tileElements = Array.from(document.getElementsByClassName("tile"));
+    tileElements.forEach((tile) => {
+      tile.removeEventListener("click", eventFunc, { once: true });
+    });
   }
 
   function reset() {
+    removeListen();
+    board = ["N", "N", "N", "N", "N", "N", "N", "N", "N"];
     document.querySelector(".choose-player").disabled = false;
     document.querySelector(".choose-player").style["background-color"] =
       "rgb(0, 193, 0)";
-      document.getElementById("res-display").innerHTML = `Let's see who is <span style="color: red;"><i>Tic - Tac - Toe</i></span> champion!`;
+    document.getElementById(
+      "res-display"
+    ).innerHTML = `Let's see who is <span style="color: red;"><i>Tic - Tac - Toe</i></span> champion!`;
   }
-
 
   return { board, createPlayers, startGame, reset };
 })();
@@ -84,7 +140,7 @@ const displayController = (() => {
     const tiles = document.getElementsByClassName("tile");
     Array.from(tiles).forEach((tile) => {
       tile.innerHTML = "";
-      tile.style['background-color'] = "#fff";
+      tile.style["background-color"] = "#fff";
     });
   }
 
@@ -95,7 +151,7 @@ const displayController = (() => {
 
   function changeTileColor(index, color) {
     const tileDiv = document.querySelector('[data-attribute="' + index + '"]');
-    tileDiv.style['background-color'] = color;
+    tileDiv.style["background-color"] = color;
   }
 
   return {
